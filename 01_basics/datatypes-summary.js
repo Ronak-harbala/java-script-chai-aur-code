1ca3bfe886c6a3482fe20e1609008f4839f524e2
Ronak <125375100+Ronak-harbala@users.noreply.github.com>
2024-04-07 12:18:29 +0000

    data type summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types : String , Number , Boolean , null
+
+// const score = 100;
+// const scoreValue = 100.2;
+
+// const isLoggedIn = true;
+// const outsideTemp = null;
+
+// let userEmail;
+// console.log(userEmail)
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+const bigNumber = 4887989999784412121n;
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+
+// console.log(id === anotherId)
+
+// Reference type(Non primitive)
+
+// Array  , Objects
+
+
+const heros = ["shaktimaan" , "naagraj" , "doga"];
+
+let myObj = {
+    name : "Ronak",
+    age : 22,
+    class : "AI3"
+}
+
+console.log(myObj.name)
+
