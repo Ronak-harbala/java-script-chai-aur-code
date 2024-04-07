7ef9d98dfb87d146797b305bc00edeb901ec8777
Ronak <125375100+Ronak-harbala@users.noreply.github.com>
2024-04-07 05:18:40 +0000

    added data types in the file

---
+++ 01_basics/data_types.js
@@ -0,0 +1,22 @@
+let name = "Ronak"
+let age = 20
+let isLoggedIn = false
+
+
+
+// number => 2 to power 32
+// bigint => 
+// string => ""
+// boolean => ture/false
+// null => standalone value and also a data type
+// undefined => abhi value assign nahi hui h
+// symbol => used to find uniqueness
+
+// -------- NON PRIMITIVE DATA TYPE
+
+// Object
+
+console.log(typeof undefined); // undefinded
+console.log(typeof null);     // object
+
+
