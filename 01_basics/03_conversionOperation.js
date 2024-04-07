f25d21617aa3decde35cbc54d5de7315b4b98fa7
Ronak <125375100+Ronak-harbala@users.noreply.github.com>
2024-04-07 05:41:59 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// // "33" => 33
+// // "33abc" => Nan  but variable type still number
+// // true =>  1
+// // null || undefinde => Nan
+
+// let isLoggedIn = undefined
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// // 1 => true
+// // "" => true
+// // any number(except 0) => true
+// // null || undefined => false
+
+let someNumber = true
+
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber)
+console.log(someNumber)
